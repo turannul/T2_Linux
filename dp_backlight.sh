c2ed4b09cef044e3ba4e3ae9a8856a6b622b2104
Turann_ <turanull000@gmail.com>
2025-12-07 14:00:43 +0300

    First commit: Refactor variable names to snake_case in shell scripts

---
+++ dp_backlight.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+brightness_input="$1"
+device_path="/sys/class/backlight/intel_backlight"
+current_brightness_raw=$(cat "$device_path/actual_brightness")
+brightness_path="$device_path/brightness"
+max_brightness=$(cat "$device_path/max_brightness")
+new_percentage=${brightness_input//%}
+
+if ! [[ "$new_percentage" =~ ^[0-9]+$ ]]; then
+    echo "Error: Invalid brightness value provided. Please use a number (e.g., 50 or 50%)." >&2
+    echo "Usage: dp-backlight <percentage>" >&2
+    exit 1
+fi
+
+current_percentage=$((current_brightness_raw * 100 / max_brightness))
+
+if [ "$new_percentage" -gt 100 ]; then
+    echo "Error: Percentage cannot be greater than 100." >&2
+    exit 1
+fi
+
+new_brightness_level=$((max_brightness * new_percentage / 100))
+
+if [ -e "$brightness_path" ]; then
+    echo "$new_brightness_level" >"$brightness_path"
+    printf "Brightness changed from %d%% (%d) to %d%% (%d)\n" "$current_percentage" "$current_brightness_raw" "$new_percentage" "$new_brightness_level"
+    exit 0
+else
+    echo "Display backlight device not found at $device_path." >&2
+    exit 1
+fi
